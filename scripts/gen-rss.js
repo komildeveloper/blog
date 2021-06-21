d23a6316e0c63fb9632067f693bc04e742f85ee3
komildeveloper <komildeveloper111@gmail.com>
2021-06-22 01:01:28 +0500

    initial commit

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'komildeveloper',
+    site_url: 'https://komildeveloper.com'
+    // feed_url: 'https://yoursite.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
